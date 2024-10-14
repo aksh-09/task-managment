2cdcda4fd0180d1bf02a221127603103408679ab
Patidar <akshita.a.patidar@capgemini.com>
2024-10-14 14:56:14 +0530

    adding env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
+require('dotenv').config({ path: `.env` });
+const host=process.env.HOST;
+const port=parseInt(process.env.PORT,10);
+const username=process.env.USERNAME;
+const password=process.env.PASSWORD;
+const db=process.env.DATABASE;
 @Module({
   imports: [
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'task-management',
+      host: host,
+      port: port,
+      username: username,
+      password: password,
+      database: db,
       autoLoadEntities: true,
       synchronize: true,
     }),
